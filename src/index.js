d810569fdc60f3200de6437749f5e756034e2637
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-21 01:19:24 +0530

    socket.io imported and a connection is made

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
+const socketio = require('socket.io')
 const path = require('path')
+const http = require('http')
 const express = require('express')
 
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 
 app.use(express.static(publicDirectoryPath))
 
+io.on('connection', () => {
+   console.log('New connection of web socket')
+})
 
 
 
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on the port ' + port)
 })
