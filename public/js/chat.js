60bc9a46946b1a0677f9be6b71179d248f3df8ac
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-23 16:59:04 +0530

    location rendered

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,14 +5,36 @@
  const $messageFormInput = $messageForm.querySelector('input')
  const $messageFormButton = $messageForm.querySelector('button')
  const $sendLocationButton = document.querySelector('#send-location')
+ const $messages = document.querySelector('#messages')
+
+
+ //templates
+ const messageTemplates = document.querySelector('#message-template').innerHTML
+ const locationTemplates = document.querySelector('#location-template').innerHTML
+
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplates, {
+        message: message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    
+
+})
+
+
+socket.on('locationMessage', (url) => {
+     console.log(url)
+     const html = Mustache.render(locationTemplates, {
+        url:url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
+  
      $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
