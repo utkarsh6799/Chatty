935bd69724172ce3aa4b98a0f053946da38b8a56
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-08-04 20:05:34 +0530

    making an add user function

--- src/utils/users.js
+++ src/utils/users.js
@@ -4,4 +4,28 @@ const users = []
 const addUser = ({ id, username, room }) => {
    //clean the data
    username = username.trim().toLowerCase()
+   room = room.trim().toLowerCase()
+
+   //Validate the Data
+   if (!username || !room) {
+       return {
+           error: 'Username and Room  are required'
+       }
+
+   }
+
+   // Check for existing user
+   const existingUser = users.find((user) => {
+      return user.room === room && user.username === username
+   })
+
+   //Validate username
+   if(existingUser) {
+      return {
+          error: 'Username in use!'
+      }
+   }
+
+   
+
 }
